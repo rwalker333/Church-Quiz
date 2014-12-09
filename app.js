04a2caa9593023e303a6a14f7da3fb8938762078
sprintup <stephen.printup@gmail.com>
2014-12-09 08:24:21 -0800

    First Commit
    Switching from treehouse workspaces to Git

---
+++ app.js
@@ -0,0 +1,12 @@
+//Problem: I would like to add a page where there are a series of quiz questions 
+//Solution: 
+
+//Problem: If they answer correctly they get a friendly message saying "alright high five!" or "Great job!" if they get it wrong they get a message saying "ok, try again" or something like that. 
+//Solution: Create and if/then pop up alert that displays the affirmative text. 
+    function (checkanswer) {
+        if (input[type="genesis"]){
+        alert("Great job!");
+    } else (alert ("Don't worry try again."));}
+
+//Problem: I want to add at least 50 - 100 questions
+//Solution: Create an Array with questions and their respective answers to be placed in the quiz question heading. 
